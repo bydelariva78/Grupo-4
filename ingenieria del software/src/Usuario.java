347b9a5fbaa3a54db93e1082d362626f830e7a9a
bydelariva78 <202204561@alu.comillas.edu>
2024-10-08 18:16:08 +0200

    Add files via upload

---
+++ ingenieria del software/src/Usuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    String nombre;
+    String contrasenya;
+
+    public Usuario(String nombre, String contrasenya){
+        this.nombre = nombre;
+        this.contrasenya = contrasenya;
+
+    }
+}
